531ef255f88b7e66f0ee5f2bf42ddffbd294b93d
Jonathan Goulet <jgoulet1980@gmail.com>
2019-11-07 22:54:27 -0500

    AddLast3games

--- NHLPredictions/PredictionSheet.Designer.cs
+++ NHLPredictions/PredictionSheet.Designer.cs
@@ -28,59 +28,78 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.lblHomeTeam = new System.Windows.Forms.Label();
-            this.lblAwayTeam = new System.Windows.Forms.Label();
-            this.lblHomeGoalie = new System.Windows.Forms.Label();
-            this.lblAwayGoalie = new System.Windows.Forms.Label();
-            this.lblHottestLast3Games = new System.Windows.Forms.Label();
+            this.lblOffHomeTeam = new System.Windows.Forms.Label();
+            this.lblOffAwayTeam = new System.Windows.Forms.Label();
+            this.lblOffHomeGoalie = new System.Windows.Forms.Label();
+            this.lblOffAwayGoalie = new System.Windows.Forms.Label();
+            this.lblHottestOffLast3Games = new System.Windows.Forms.Label();
             this.lblGfLast3Home = new System.Windows.Forms.Label();
             this.lblGfLast3Away = new System.Windows.Forms.Label();
+            this.txtOffHomeTeam = new System.Windows.Forms.TextBox();
+            this.txtOffAwayTeam = new System.Windows.Forms.TextBox();
+            this.txtOffAwayGoalie = new System.Windows.Forms.TextBox();
+            this.txtAwayGFLast3 = new System.Windows.Forms.TextBox();
+            this.txtOffHomeGoalie = new System.Windows.Forms.TextBox();
+            this.txtHomeGFLast3 = new System.Windows.Forms.TextBox();
+            this.txtHomeGALast3 = new System.Windows.Forms.TextBox();
+            this.txtDefHomeGoalie = new System.Windows.Forms.TextBox();
+            this.txtAwayGALast3 = new System.Windows.Forms.TextBox();
+            this.txtDefAwayGoalie = new System.Windows.Forms.TextBox();
+            this.txtDefAwayTeam = new System.Windows.Forms.TextBox();
+            this.txtDefHomeTeam = new System.Windows.Forms.TextBox();
+            this.lblGaLast3Away = new System.Windows.Forms.Label();
+            this.lblGaLast3Home = new System.Windows.Forms.Label();
+            this.lblHottestDefLast3Games = new System.Windows.Forms.Label();
+            this.lblDefAwayGoalie = new System.Windows.Forms.Label();
+            this.lblDefHomeGoalie = new System.Windows.Forms.Label();
+            this.lblDefAwayTeam = new System.Windows.Forms.Label();
+            this.lblDefHomeTeam = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // lblHomeTeam
+            // lblOffHomeTeam
             // 
-            this.lblHomeTeam.AutoSize = true;
-            this.lblHomeTeam.Location = new System.Drawing.Point(32, 95);
-            this.lblHomeTeam.Name = "lblHomeTeam";
-            this.lblHomeTeam.Size = new System.Drawing.Size(71, 13);
-            this.lblHomeTeam.TabIndex = 0;
-            this.lblHomeTeam.Text = "Home Team :";
+            this.lblOffHomeTeam.AutoSize = true;
+            this.lblOffHomeTeam.Location = new System.Drawing.Point(32, 95);
+            this.lblOffHomeTeam.Name = "lblOffHomeTeam";
+            this.lblOffHomeTeam.Size = new System.Drawing.Size(71, 13);
+            this.lblOffHomeTeam.TabIndex = 0;
+            this.lblOffHomeTeam.Text = "Home Team :";
             // 
-            // lblAwayTeam
+            // lblOffAwayTeam
             // 
-            this.lblAwayTeam.AutoSize = true;
-            this.lblAwayTeam.Location = new System.Drawing.Point(291, 95);
-            this.lblAwayTeam.Name = "lblAwayTeam";
-            this.lblAwayTeam.Size = new System.Drawing.Size(69, 13);
-            this.lblAwayTeam.TabIndex = 1;
-            this.lblAwayTeam.Text = "Away Team :";
+            this.lblOffAwayTeam.AutoSize = true;
+            this.lblOffAwayTeam.Location = new System.Drawing.Point(291, 95);
+            this.lblOffAwayTeam.Name = "lblOffAwayTeam";
+            this.lblOffAwayTeam.Size = new System.Drawing.Size(69, 13);
+            this.lblOffAwayTeam.TabIndex = 1;
+            this.lblOffAwayTeam.Text = "Away Team :";
             // 
-            // lblHomeGoalie
+            // lblOffHomeGoalie
             // 
-            this.lblHomeGoalie.AutoSize = true;
-            this.lblHomeGoalie.Location = new System.Drawing.Point(33, 128);
-            this.lblHomeGoalie.Name = "lblHomeGoalie";
-            this.lblHomeGoalie.Size = new System.Drawing.Size(74, 13);
-            this.lblHomeGoalie.TabIndex = 2;
-            this.lblHomeGoalie.Text = "Home Goalie :";
+            this.lblOffHomeGoalie.AutoSize = true;
+            this.lblOffHomeGoalie.Location = new System.Drawing.Point(33, 128);
+            this.lblOffHomeGoalie.Name = "lblOffHomeGoalie";
+            this.lblOffHomeGoalie.Size = new System.Drawing.Size(74, 13);
+            this.lblOffHomeGoalie.TabIndex = 2;
+            this.lblOffHomeGoalie.Text = "Home Goalie :";
             // 
-            // lblAwayGoalie
+            // lblOffAwayGoalie
             // 
-            this.lblAwayGoalie.AutoSize = true;
-            this.lblAwayGoalie.Location = new System.Drawing.Point(291, 128);
-            this.lblAwayGoalie.Name = "lblAwayGoalie";
-            this.lblAwayGoalie.Size = new System.Drawing.Size(72, 13);
-            this.lblAwayGoalie.TabIndex = 3;
-            this.lblAwayGoalie.Text = "Away Goalie :";
+            this.lblOffAwayGoalie.AutoSize = true;
+            this.lblOffAwayGoalie.Location = new System.Drawing.Point(291, 128);
+            this.lblOffAwayGoalie.Name = "lblOffAwayGoalie";
+            this.lblOffAwayGoalie.Size = new System.Drawing.Size(72, 13);
+            this.lblOffAwayGoalie.TabIndex = 3;
+            this.lblOffAwayGoalie.Text = "Away Goalie :";
             // 
-            // lblHottestLast3Games
+            // lblHottestOffLast3Games
             // 
-            this.lblHottestLast3Games.AutoSize = true;
-            this.lblHottestLast3Games.Location = new System.Drawing.Point(112, 35);
-            this.lblHottestLast3Games.Name = "lblHottestLast3Games";
-            this.lblHottestLast3Games.Size = new System.Drawing.Size(182, 13);
-            this.lblHottestLast3Games.TabIndex = 4;
-            this.lblHottestLast3Games.Text = "Hottest Offense Teams Last 3 games";
+            this.lblHottestOffLast3Games.AutoSize = true;
+            this.lblHottestOffLast3Games.Location = new System.Drawing.Point(112, 35);
+            this.lblHottestOffLast3Games.Name = "lblHottestOffLast3Games";
+            this.lblHottestOffLast3Games.Size = new System.Drawing.Size(182, 13);
+            this.lblHottestOffLast3Games.TabIndex = 4;
+            this.lblHottestOffLast3Games.Text = "Hottest Offense Teams Last 3 games";
             // 
             // lblGfLast3Home
             // 
@@ -100,18 +119,184 @@
             this.lblGfLast3Away.TabIndex = 6;
             this.lblGfLast3Away.Text = "GF/G last 3 Games :";
             // 
+            // txtOffHomeTeam
+            // 
+            this.txtOffHomeTeam.Location = new System.Drawing.Point(114, 93);
+            this.txtOffHomeTeam.Name = "txtOffHomeTeam";
+            this.txtOffHomeTeam.Size = new System.Drawing.Size(133, 20);
+            this.txtOffHomeTeam.TabIndex = 7;
+            // 
+            // txtOffAwayTeam
+            // 
+            this.txtOffAwayTeam.Location = new System.Drawing.Point(366, 92);
+            this.txtOffAwayTeam.Name = "txtOffAwayTeam";
+            this.txtOffAwayTeam.Size = new System.Drawing.Size(135, 20);
+            this.txtOffAwayTeam.TabIndex = 8;
+            // 
+            // txtOffAwayGoalie
+            // 
+            this.txtOffAwayGoalie.Location = new System.Drawing.Point(369, 125);
+            this.txtOffAwayGoalie.Name = "txtOffAwayGoalie";
+            this.txtOffAwayGoalie.Size = new System.Drawing.Size(132, 20);
+            this.txtOffAwayGoalie.TabIndex = 9;
+            // 
+            // txtAwayGFLast3
+            // 
+            this.txtAwayGFLast3.Location = new System.Drawing.Point(401, 155);
+            this.txtAwayGFLast3.Name = "txtAwayGFLast3";
+            this.txtAwayGFLast3.Size = new System.Drawing.Size(100, 20);
+            this.txtAwayGFLast3.TabIndex = 10;
+            // 
+            // txtOffHomeGoalie
+            // 
+            this.txtOffHomeGoalie.Location = new System.Drawing.Point(113, 125);
+            this.txtOffHomeGoalie.Name = "txtOffHomeGoalie";
+            this.txtOffHomeGoalie.Size = new System.Drawing.Size(134, 20);
+            this.txtOffHomeGoalie.TabIndex = 11;
+            // 
+            // txtHomeGFLast3
+            // 
+            this.txtHomeGFLast3.Location = new System.Drawing.Point(147, 155);
+            this.txtHomeGFLast3.Name = "txtHomeGFLast3";
+            this.txtHomeGFLast3.Size = new System.Drawing.Size(100, 20);
+            this.txtHomeGFLast3.TabIndex = 12;
+            // 
+            // txtHomeGALast3
+            // 
+            this.txtHomeGALast3.Location = new System.Drawing.Point(735, 155);
+            this.txtHomeGALast3.Name = "txtHomeGALast3";
+            this.txtHomeGALast3.Size = new System.Drawing.Size(100, 20);
+            this.txtHomeGALast3.TabIndex = 25;
+            // 
+            // txtDefHomeGoalie
+            // 
+            this.txtDefHomeGoalie.Location = new System.Drawing.Point(701, 125);
+            this.txtDefHomeGoalie.Name = "txtDefHomeGoalie";
+            this.txtDefHomeGoalie.Size = new System.Drawing.Size(134, 20);
+            this.txtDefHomeGoalie.TabIndex = 24;
+            // 
+            // txtAwayGALast3
+            // 
+            this.txtAwayGALast3.Location = new System.Drawing.Point(989, 155);
+            this.txtAwayGALast3.Name = "txtAwayGALast3";
+            this.txtAwayGALast3.Size = new System.Drawing.Size(100, 20);
+            this.txtAwayGALast3.TabIndex = 23;
+            // 
+            // txtDefAwayGoalie
+            // 
+            this.txtDefAwayGoalie.Location = new System.Drawing.Point(957, 125);
+            this.txtDefAwayGoalie.Name = "txtDefAwayGoalie";
+            this.txtDefAwayGoalie.Size = new System.Drawing.Size(132, 20);
+            this.txtDefAwayGoalie.TabIndex = 22;
+            // 
+            // txtDefAwayTeam
+            // 
+            this.txtDefAwayTeam.Location = new System.Drawing.Point(954, 92);
+            this.txtDefAwayTeam.Name = "txtDefAwayTeam";
+            this.txtDefAwayTeam.Size = new System.Drawing.Size(135, 20);
+            this.txtDefAwayTeam.TabIndex = 21;
+            // 
+            // txtDefHomeTeam
+            // 
+            this.txtDefHomeTeam.Location = new System.Drawing.Point(702, 93);
+            this.txtDefHomeTeam.Name = "txtDefHomeTeam";
+            this.txtDefHomeTeam.Size = new System.Drawing.Size(133, 20);
+            this.txtDefHomeTeam.TabIndex = 20;
+            // 
+            // lblGaLast3Away
+            // 
+            this.lblGaLast3Away.AutoSize = true;
+            this.lblGaLast3Away.Location = new System.Drawing.Point(879, 158);
+            this.lblGaLast3Away.Name = "lblGaLast3Away";
+            this.lblGaLast3Away.Size = new System.Drawing.Size(103, 13);
+            this.lblGaLast3Away.TabIndex = 19;
+            this.lblGaLast3Away.Text = "GF/A last 3 Games :";
+            // 
+            // lblGaLast3Home
+            // 
+            this.lblGaLast3Home.AutoSize = true;
+            this.lblGaLast3Home.Location = new System.Drawing.Point(625, 158);
+            this.lblGaLast3Home.Name = "lblGaLast3Home";
+            this.lblGaLast3Home.Size = new System.Drawing.Size(103, 13);
+            this.lblGaLast3Home.TabIndex = 18;
+            this.lblGaLast3Home.Text = "GF/A last 3 Games :";
+            // 
+            // lblHottestDefLast3Games
+            // 
+            this.lblHottestDefLast3Games.AutoSize = true;
+            this.lblHottestDefLast3Games.Location = new System.Drawing.Point(700, 35);
+            this.lblHottestDefLast3Games.Name = "lblHottestDefLast3Games";
+            this.lblHottestDefLast3Games.Size = new System.Drawing.Size(185, 13);
+            this.lblHottestDefLast3Games.TabIndex = 17;
+            this.lblHottestDefLast3Games.Text = "Hottest Defense Teams Last 3 games";
+            // 
+            // lblDefAwayGoalie
+            // 
+            this.lblDefAwayGoalie.AutoSize = true;
+            this.lblDefAwayGoalie.Location = new System.Drawing.Point(879, 128);
+            this.lblDefAwayGoalie.Name = "lblDefAwayGoalie";
+            this.lblDefAwayGoalie.Size = new System.Drawing.Size(72, 13);
+            this.lblDefAwayGoalie.TabIndex = 16;
+            this.lblDefAwayGoalie.Text = "Away Goalie :";
+            // 
+            // lblDefHomeGoalie
+            // 
+            this.lblDefHomeGoalie.AutoSize = true;
+            this.lblDefHomeGoalie.Location = new System.Drawing.Point(621, 128);
+            this.lblDefHomeGoalie.Name = "lblDefHomeGoalie";
+            this.lblDefHomeGoalie.Size = new System.Drawing.Size(74, 13);
+            this.lblDefHomeGoalie.TabIndex = 15;
+            this.lblDefHomeGoalie.Text = "Home Goalie :";
+            // 
+            // lblDefAwayTeam
+            // 
+            this.lblDefAwayTeam.AutoSize = true;
+            this.lblDefAwayTeam.Location = new System.Drawing.Point(879, 95);
+            this.lblDefAwayTeam.Name = "lblDefAwayTeam";
+            this.lblDefAwayTeam.Size = new System.Drawing.Size(69, 13);
+            this.lblDefAwayTeam.TabIndex = 14;
+            this.lblDefAwayTeam.Text = "Away Team :";
+            // 
+            // lblDefHomeTeam
+            // 
+            this.lblDefHomeTeam.AutoSize = true;
+            this.lblDefHomeTeam.Location = new System.Drawing.Point(620, 95);
+            this.lblDefHomeTeam.Name = "lblDefHomeTeam";
+            this.lblDefHomeTeam.Size = new System.Drawing.Size(71, 13);
+            this.lblDefHomeTeam.TabIndex = 13;
+            this.lblDefHomeTeam.Text = "Home Team :";
+            // 
             // PredictionSheet
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1221, 631);
+            this.Controls.Add(this.txtHomeGALast3);
+            this.Controls.Add(this.txtDefHomeGoalie);
+            this.Controls.Add(this.txtAwayGALast3);
+            this.Controls.Add(this.txtDefAwayGoalie);
+            this.Controls.Add(this.txtDefAwayTeam);
+            this.Controls.Add(this.txtDefHomeTeam);
+            this.Controls.Add(this.lblGaLast3Away);
+            this.Controls.Add(this.lblGaLast3Home);
+            this.Controls.Add(this.lblHottestDefLast3Games);
+            this.Controls.Add(this.lblDefAwayGoalie);
+            this.Controls.Add(this.lblDefHomeGoalie);
+            this.Controls.Add(this.lblDefAwayTeam);
+            this.Controls.Add(this.lblDefHomeTeam);
+            this.Controls.Add(this.txtHomeGFLast3);
+            this.Controls.Add(this.txtOffHomeGoalie);
+            this.Controls.Add(this.txtAwayGFLast3);
+            this.Controls.Add(this.txtOffAwayGoalie);
+            this.Controls.Add(this.txtOffAwayTeam);
+            this.Controls.Add(this.txtOffHomeTeam);
             this.Controls.Add(this.lblGfLast3Away);
             this.Controls.Add(this.lblGfLast3Home);
-            this.Controls.Add(this.lblHottestLast3Games);
-            this.Controls.Add(this.lblAwayGoalie);
-            this.Controls.Add(this.lblHomeGoalie);
-            this.Controls.Add(this.lblAwayTeam);
-            this.Controls.Add(this.lblHomeTeam);
+            this.Controls.Add(this.lblHottestOffLast3Games);
+            this.Controls.Add(this.lblOffAwayGoalie);
+            this.Controls.Add(this.lblOffHomeGoalie);
+            this.Controls.Add(this.lblOffAwayTeam);
+            this.Controls.Add(this.lblOffHomeTeam);
             this.Name = "PredictionSheet";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -121,13 +306,32 @@
 
         #endregion
 
-        private System.Windows.Forms.Label lblHomeTeam;
-        private System.Windows.Forms.Label lblAwayTeam;
-        private System.Windows.Forms.Label lblHomeGoalie;
-        private System.Windows.Forms.Label lblAwayGoalie;
-        private System.Windows.Forms.Label lblHottestLast3Games;
+        private System.Windows.Forms.Label lblOffHomeTeam;
+        private System.Windows.Forms.Label lblOffAwayTeam;
+        private System.Windows.Forms.Label lblOffHomeGoalie;
+        private System.Windows.Forms.Label lblOffAwayGoalie;
+        private System.Windows.Forms.Label lblHottestOffLast3Games;
         private System.Windows.Forms.Label lblGfLast3Home;
         private System.Windows.Forms.Label lblGfLast3Away;
+        private System.Windows.Forms.TextBox txtOffHomeTeam;
+        private System.Windows.Forms.TextBox txtOffAwayTeam;
+        private System.Windows.Forms.TextBox txtOffAwayGoalie;
+        private System.Windows.Forms.TextBox txtAwayGFLast3;
+        private System.Windows.Forms.TextBox txtOffHomeGoalie;
+        private System.Windows.Forms.TextBox txtHomeGFLast3;
+        private System.Windows.Forms.TextBox txtHomeGALast3;
+        private System.Windows.Forms.TextBox txtDefHomeGoalie;
+        private System.Windows.Forms.TextBox txtAwayGALast3;
+        private System.Windows.Forms.TextBox txtDefAwayGoalie;
+        private System.Windows.Forms.TextBox txtDefAwayTeam;
+        private System.Windows.Forms.TextBox txtDefHomeTeam;
+        private System.Windows.Forms.Label lblGaLast3Away;
+        private System.Windows.Forms.Label lblGaLast3Home;
+        private System.Windows.Forms.Label lblHottestDefLast3Games;
+        private System.Windows.Forms.Label lblDefAwayGoalie;
+        private System.Windows.Forms.Label lblDefHomeGoalie;
+        private System.Windows.Forms.Label lblDefAwayTeam;
+        private System.Windows.Forms.Label lblDefHomeTeam;
     }
 }
 
